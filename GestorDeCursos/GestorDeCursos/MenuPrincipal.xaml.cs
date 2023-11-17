3d508e162b447163de1bfeb5fa4d1bb088299208
Jesus Flores <Jesus Flores@DESKTOP-FV3T3LV>
2023-11-17 02:28:48 -0600

    Agregar archivos de proyecto.

---
+++ GestorDeCursos/GestorDeCursos/MenuPrincipal.xaml.cs
@@ -0,0 +1,34 @@
+﻿using GestorDeCursos.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GestorDeCursos
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MenuPrincipal : ContentPage
+    {
+        public MenuPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private async void PaginaEmpleado_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new CapturarFoto());
+        }
+        private async void PaginaCurso_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Cursos());
+        }
+        private async void PaginaSeguimiento_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new SeguimientosDeCursoPage());
+        }
+    }
+}
---
+++ GestorDeCursos/GestorDeCursos/Registro.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using GestorDeCursos.Models;
+
+using Xamarin.Forms;
+
+namespace GestorDeCursos
+{	
+	public partial class Registro : ContentPage
+	{	
+		public Registro ()
+		{
+			InitializeComponent ();
+		}
+
+        private async void Btn_Registrar(System.Object sender, System.EventArgs e)
+        {
+
+            if (string.IsNullOrEmpty(txtEmailReg.Text))
+            {
+                await DisplayAlert("AVISO", "El campo de email está vacio", "OK");
+                return;
+
+            }
+
+            if (string.IsNullOrEmpty(txtContraReg.Text))
+            {
+                await DisplayAlert("AVISO", "El campo de contraseña está vacio", "OK");
+                return;
+
+            }
+
+            if (string.IsNullOrEmpty(txtNombreReg.Text))
+            {
+                await DisplayAlert("AVISO", "El campo de nombre está vacio", "OK");
+                return;
+
+            }
+
+            if (string.IsNullOrEmpty(txtEdadReg.Text))
+            {
+				await DisplayAlert("AVISO", "El campo de edad está vacio", "OK");
+				return;
+
+            }
+
+            Users usr = new Users()
+            {
+                Email = txtEmailReg.Text,
+                Emailpassword = txtContraReg.Text,
+                NombreCompleto = txtNombreReg.Text,
+                Edad = int.Parse(txtEdadReg.Text),
+                FechaCreacion = DateTime.Now
+
+            };
+
+            await App.SQLiteDB.SaveUserModelAsync(usr);
+			await DisplayAlert("AVISO", "¡Usuario registrado con exito!", "OK");
+
+			txtEmailReg.Text = "";
+			txtContraReg.Text = "";
+			txtNombreReg.Text = "";
+			txtEdadReg.Text = "";
+
+			await Navigation.PushAsync(new Login());
+        }
+    }
+}
+
